8b596ad2205fe67afd27bbefcccecb4724d6f90e
stenkarl <sten_karl@yahoo.com>
2024-04-21 21:00:53 -0500

    day 15 part 2

--- kotlin/adventofcode2023/src/main/kotlin/day15/Main.kt
+++ kotlin/adventofcode2023/src/main/kotlin/day15/Main.kt
@@ -1,6 +1,16 @@
 package day15
 
-val codes = java.io.File("/Users/sten/dev/adventofcode2023/kotlin/adventofcode2023/src/main/resources/day15sample.txt").readLines().first().split(",")
+private val codes = java.io.File("/Users/sten/dev/adventofcode2023/kotlin/adventofcode2023/src/main/resources/day15.txt").readLines().first().split(",")
+private val boxes = Array<MutableList<Lens>>(256) { mutableListOf() }
+
+private data class Lens(val label:String, val op:Char, val focal:Int = 0) {
+    val hash = hash(label)
+
+    override fun toString(): String {
+        return "Lens($label, $op, $focal, $hash)"
+    }
+}
+
 
 fun main() {
     println(codes)
@@ -18,11 +28,56 @@ private fun part1() {
 }
 
 private fun part2() {
-    println("")
+    val lenses = codes.map {
+        if (it.contains('=')) {
+            Lens(it.substring(0, it.length - 2), it[it.length - 2], it[it.length - 1].digitToInt())
+        } else {
+            Lens(it.substring(0, it.length - 1), it[it.length - 1])
+        }
+    }
+    println(lenses)
+
+    lenses.forEach { lens ->
+        apply(lens)
+    }
+    printBoxes()
+
+    val sum = boxes.mapIndexed { boxIndex, list ->
+        list.mapIndexed { index, lens ->
+            (boxIndex + 1) * (index + 1) * lens.focal
+        }.sum()
+    }.sum()
+
+    println("Sum $sum")
+}
+
+private fun apply(lens:Lens) {
+    val list = boxes[lens.hash]
+    when (lens.op) {
+        '-' -> list.removeIf { it.label == lens.label }
+        '=' -> {
+            val filtered = list.filter { it.label == lens.label }
+            if (filtered.isNotEmpty()) {
+                val first = filtered.first()
+                val idx = list.indexOf(first)
+                list.add(idx, lens)
+                list.remove(first)
+            } else {
+                list.add(lens)
+            }
+        }
+
+
+    }
+}
+
+private fun printBoxes() {
+    boxes.forEachIndexed { index, list ->
+        if (list.isNotEmpty()) println("$index: $list")
+    }
 }
 
 private fun hash(str:String) =
     str.fold(0) { acc, ch ->
-        //println("$ch, acc: $acc, code: ${ch.code}")
         (acc + ch.code) * 17 % 256
     }
